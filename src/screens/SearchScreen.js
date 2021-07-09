d1576b6ea5acfa39ce743faef31a66ecb45460ca
tomb-cinch <tom.bilbe@cinch.co.uk>
2021-07-09 14:15:26 +0100

    initial screen and navigator linked up - check the impport methods in app.js to get around some simple errors

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search screen</Text>
+    </View>
+  );
+};
+
+export default SearchScreen;
+
+const styles = StyleSheet.create({});
