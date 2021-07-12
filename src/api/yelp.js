aa2246f59fae67adf676eb6d9624556546de3fc7
tomb-cinch <tom.bilbe@cinch.co.uk>
2021-07-12 09:17:59 +0100

    removed bearer file

--- src/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.yelp.com/v3/businesses",
-  headers: {
-    Authorization: `Bearer N19uR0k-U53tbBD9hKpmR8XHiwG_YIOyThf4CMsiOQ_WaRWRRPQNfeC_s58rIq3vZ5SXEh80ZGlB-GRt7HxQRTA0PtB15gWIKzuj88XQ2CJTCHMrfmOIDp1xqkDoYHYx`,
-  },
-});
