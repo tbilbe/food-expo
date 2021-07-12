4ee42292628637e6f750d8d5e2212c1cd1640d69
tomb-cinch <tom.bilbe@cinch.co.uk>
2021-07-12 09:01:01 +0100

    update bearer

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
-    Authorization: `Bearer N29uR0k-U53tbBD9hKpmR8XHiwG_YIOyThf4CMsiOQ_WaRWRRPQNfeC_s58rIq3vZ5SXEh80ZGlB-GRt7HxQRTA0PtB15gWIKzuj88XQ2CJTCHMrfmOIDp1xqkDoYHYx`,
+    Authorization: `Bearer N19uR0k-U53tbBD9hKpmR8XHiwG_YIOyThf4CMsiOQ_WaRWRRPQNfeC_s58rIq3vZ5SXEh80ZGlB-GRt7HxQRTA0PtB15gWIKzuj88XQ2CJTCHMrfmOIDp1xqkDoYHYx`,
   },
 });
